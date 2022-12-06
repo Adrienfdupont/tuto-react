3d2e185b69f5a065928a8f886f98fea1f9622a1c
Adrienfdupont <adrienf.dupont@gmail.com>
2022-12-06 16:02:17 +0100

    polices installées

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
+    <header className="text-white font-lato text-xl">
       <nav className="flex flex-col justify-center bg-red-700 h-16">
         <ul className="flex justify-around items-center">
           <li>
-            <h1>Avec les doigts</h1>
+            <h1 className="font-sacramento text-4xl">Avec les doigts</h1>
           </li>
           <div className="flex items-center">
             <li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sacramento: ["Sacramento", "cursive"],
+        lato: ["Lato", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
